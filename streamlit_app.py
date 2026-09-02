acc23b0d38730b25f997213f85f69b7c070705df
I-ZHI CHEN <81600836+ChairChen@users.noreply.github.com>
2026-09-02 16:09:26 -0500

    Update streamlit_app.py
    Updated the smoothiefroot api call for each fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -25,13 +26,17 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ' '.join(ingredients_list)
-    # st.write(ingredients_string)
-
+  
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get('https://my.smoothiefroot.com/api/fruit/'+fruit_chosen)
+        sf_df = st.datafrom(data=smoothiefroot_response.json(), use_container_width=True)
+  
     my_insert_stmt = """
         insert into smoothies.public.orders(ingredients, name_on_order)
         values('"""+ingredients_string+"""','"""+name_on_order+"""')
     """
-    # st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
 
